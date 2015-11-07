be8771b2a5441e816f178e87659cdb2cd7b9e012
Kate Duddy <kaduddy@gmail.com>
2015-11-06 18:08:48 -0800

    adds ninja(color)

--- server.py
+++ server.py
@@ -3,12 +3,24 @@ app = Flask(__name__)
 app.secret_key = 'ThisIsSecret'
 @app.route(/)
 def index():
-	print "No ninjas here"
+	return render_template('index.html')
 @app.route(/ninja)
+def turtles():
+	return render_template('ninjas.html')
 @app.route(/ninja/<color>)
 def ninja(color):
+	if color == 'blue':
+		return render_template('blue.html')
+	elif color == 'orange':
+		return render_template('orange.html')
+	elif color == 'purple':
+		return render_template('purple.html')
+	elif color == 'red':
+		return render_template('red.html')
+	else:
+		return render_template('fox.html')
 
 #four color options
-@app.route(/ninja/)
+
 
 app.run(debug=True)
