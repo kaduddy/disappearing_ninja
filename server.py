0a5a7705baaac0e4b472a00bc82cfd68379574b1
Kate Duddy <kaduddy@gmail.com>
2015-11-06 18:17:58 -0800

    adds images

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, redirect, request, session, flash
 app = Flask(__name__)
 app.secret_key = 'ThisIsSecret'
-@app.route(/)
+@app.route('/')
 def index():
 	return render_template('index.html')
-@app.route(/ninja)
+@app.route('/ninja')
 def turtles():
 	return render_template('ninjas.html')
-@app.route(/ninja/<color>)
+@app.route('/ninja/<color>')
 def ninja(color):
 	if color == 'blue':
 		return render_template('blue.html')
@@ -19,8 +19,6 @@ def ninja(color):
 		return render_template('red.html')
 	else:
 		return render_template('fox.html')
-
-#four color options
 
 
 app.run(debug=True)
